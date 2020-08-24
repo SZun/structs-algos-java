52cdcf8a0c1d8728ec231bee24a1e3456cce3715
Zun <zunsamuel97@gmail.com>
2020-08-24 02:39:02 -0500

    singly linked lists

--- src/com/zun/Main.java
+++ src/com/zun/Main.java
@@ -8,10 +8,23 @@ import java.util.Vector;
 public class Main {
 
     public static void main(String[] args) {
-        List<Employee> employeeList = new Vector<>();
-        employeeList.add(new Employee(123, "Jane", "Jones"));
-        employeeList.add(new Employee(4567, "John", "Doe"));
-        employeeList.add(new Employee(22, "Mary", "Smith"));
-        employeeList.add(new Employee(3245, "Mike", "Wilson"));
+        Employee employee1 = new Employee(123, "Jane", "Jones");
+        Employee employee2 = new Employee(4567, "John", "Doe");
+        Employee employee3 = new Employee(22, "Mary", "Smith");
+        Employee employee4 = new Employee(3245, "Mike", "Wilson");
+
+        EmployeeLinkedList list = new EmployeeLinkedList();
+
+        list.addToFront(employee1);
+        list.addToFront(employee2);
+        list.addToFront(employee3);
+        list.addToFront(employee4);
+
+        list.print();
+        System.out.println("Size of EmployeeLinkedList is: " + list.getSize());
+        list.removeFromFront();
+        list.print();
+        System.out.println("Size of EmployeeLinkedList is: " + list.getSize());
+
     }
 }
