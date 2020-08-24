9f353a31370cbd01284d3a83a000a1678961c5ad
Zun <zunsamuel97@gmail.com>
2020-08-24 04:05:23 -0500

    lined list stack

---
+++ src/com/zun/LinkedStack.java
@@ -0,0 +1,38 @@
+package com.zun;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class LinkedStack {
+
+    private LinkedList<Employee> stack;
+
+    public LinkedStack() {
+        stack = new LinkedList<>();
+    }
+
+    public void push(Employee employee){
+        stack.push(employee);
+    }
+
+    public Employee pop(){
+        return stack.pop();
+    }
+
+    public Employee peek(){
+        return stack.peek();
+    }
+
+    public boolean isEmpty(){
+        return stack.isEmpty();
+    }
+
+    public void print(){
+        Iterator<Employee> iterator = stack.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+
+    }
+
+}
--- src/com/zun/Main.java
+++ src/com/zun/Main.java
@@ -1,22 +1,44 @@
 package com.zun;
 
+import java.util.Iterator;
+import java.util.Stack;
+
 public class Main {
 
     public static void main(String[] args) {
-	    ArrayStack stack = new ArrayStack(10);
+		Employee employee = new Employee(123, "Jane", "Jones");
+		Employee employee1 = new Employee(4567, "John", "Doe");
+		Employee employee2 = new Employee(22, "Mary", "Smith");
+		Employee employee3 = new Employee(3245, "Mike", "Wilson");
+		Employee employee4 = new Employee(78, "Bill", "Nye");
 
-	    stack.push(new Employee(123, "Jane", "Jones"));
-	    stack.push(new Employee(4567, "John", "Doe"));
-	    stack.push(new Employee(22, "Mary", "Smith"));
-	    stack.push(new Employee(3245, "Mike", "Wilson"));
-	    stack.push(new Employee(78, "Bill", "Nye"));
+		LinkedStack stack = new LinkedStack();
 
-	    stack.printStack();
+		stack.push(employee);
+		stack.push(employee1);
+		stack.push(employee2);
+		stack.push(employee3);
+		stack.push(employee4);
+
+//	    stack.print();
 
 		System.out.println(stack.peek());
 
 		System.out.println("Popped: " + stack.pop());
-
+//
 		System.out.println(stack.peek());
     }
+
+    public static boolean checkIsPalindrom(String s){
+		Stack stack = new Stack();
+		String res = "";
+    	for(String i : s.split("")){
+    		stack.push(i);
+		}
+		Iterator iterator = stack.iterator();
+    	while(iterator.hasNext()){
+    		res += iterator.next();
+		}
+    	return s.equals(res);
+	}
 }
