a42c26f2dbe69e044a1b18ce144b6d36757512f8
Zun <zunsamuel97@gmail.com>
2020-08-24 05:05:09 -0500

    linear search

--- src/com/zun/Main.java
+++ src/com/zun/Main.java
@@ -3,6 +3,15 @@ package com.zun;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int[] arr = {20, 35, -15, 7, 55, 1, -22};
+
+        System.out.println(linearSearch(arr, -15));
+    }
+
+    private static int linearSearch(int[] arr, int value){
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == value) return i;
+        }
+        return -1;
     }
 }
