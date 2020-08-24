c365b26479003bc07f8769355d078760c5a86652
Zun <zunsamuel97@gmail.com>
2020-08-24 03:50:25 -0500

    array stack

--- src/com/zun/Main.java
+++ src/com/zun/Main.java
@@ -3,6 +3,20 @@ package com.zun;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    ArrayStack stack = new ArrayStack(10);
+
+	    stack.push(new Employee(123, "Jane", "Jones"));
+	    stack.push(new Employee(4567, "John", "Doe"));
+	    stack.push(new Employee(22, "Mary", "Smith"));
+	    stack.push(new Employee(3245, "Mike", "Wilson"));
+	    stack.push(new Employee(78, "Bill", "Nye"));
+
+	    stack.printStack();
+
+		System.out.println(stack.peek());
+
+		System.out.println("Popped: " + stack.pop());
+
+		System.out.println(stack.peek());
     }
 }
