689b5c0df916ea87831da0b3ca24d884d1c922bc
Zun <zunsamuel97@gmail.com>
2020-08-24 02:19:37 -0500

    vector

--- src/com/zun/Main.java
+++ src/com/zun/Main.java
@@ -3,22 +3,15 @@ package com.zun;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Vector;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<Employee> employeeList = new ArrayList<>();
+        List<Employee> employeeList = new Vector<>();
         employeeList.add(new Employee(123, "Jane", "Jones"));
         employeeList.add(new Employee(4567, "John", "Doe"));
         employeeList.add(new Employee(22, "Mary", "Smith"));
         employeeList.add(new Employee(3245, "Mike", "Wilson"));
-
-        employeeList.add(3, new Employee(4567, "John", "Doe"));
-
-//        employeeList.forEach(System.out::println);
-//        System.out.println(employeeList.get(1));
-
-        Employee[] employees = employeeList.toArray(Employee[]::new);
-        System.out.println(Arrays.toString(employees));
     }
 }
