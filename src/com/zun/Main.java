ba600788a45a83a63ef6068b7db2bfbb05533b2f
Zun <zunsamuel97@gmail.com>
2020-08-24 04:08:09 -0500

    stack challenge

--- src/com/zun/Main.java
+++ src/com/zun/Main.java
@@ -6,27 +6,8 @@ import java.util.Stack;
 public class Main {
 
     public static void main(String[] args) {
-		Employee employee = new Employee(123, "Jane", "Jones");
-		Employee employee1 = new Employee(4567, "John", "Doe");
-		Employee employee2 = new Employee(22, "Mary", "Smith");
-		Employee employee3 = new Employee(3245, "Mike", "Wilson");
-		Employee employee4 = new Employee(78, "Bill", "Nye");
-
-		LinkedStack stack = new LinkedStack();
-
-		stack.push(employee);
-		stack.push(employee1);
-		stack.push(employee2);
-		stack.push(employee3);
-		stack.push(employee4);
-
-//	    stack.print();
-
-		System.out.println(stack.peek());
-
-		System.out.println("Popped: " + stack.pop());
-//
-		System.out.println(stack.peek());
+		System.out.println(checkIsPalindrom("racecar"));
+		System.out.println(checkIsPalindrom("I am not a palindrome"));
     }
 
     public static boolean checkIsPalindrom(String s){
@@ -37,7 +18,7 @@ public class Main {
 		}
 		Iterator iterator = stack.iterator();
     	while(iterator.hasNext()){
-    		res += iterator.next();
+    		res += stack.pop();
 		}
     	return s.equals(res);
 	}
