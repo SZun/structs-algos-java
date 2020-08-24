90490b8e89abaee86905ed9e4c1fad199131f012
Zun <zunsamuel97@gmail.com>
2020-08-24 00:57:31 -0500

    jdk sorts

--- src/com/zun/Main.java
+++ src/com/zun/Main.java
@@ -6,49 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] intArr = {4725, 4586, 1330, 8792, 1594, 5729};
+        int[] intArr = {20, 35, -15, 7, 55, 1, -22};
 
-        radixSort(intArr, 10, 4);
+        Arrays.sort(intArr); // for smaller arrays
+        Arrays.parallelSort(intArr); // for larger data sets
 
         System.out.println(Arrays.toString(intArr));
     }
-
-    public static void radixSort(int[] input, int radix, int width) {
-        for (int i = 0; i < width; i++) {
-            radixSingleSort(input, i, radix);
-        }
-    }
-
-    public static void radixSingleSort(int[] input, int position, int radix) {
-
-        int numItems = input.length;
-        int[] countArray = new int[radix];
-
-        for (int value: input) {
-            countArray[getDigit(position, value, radix)]++;
-        }
-        // Adjust the count array
-        for (int j = 1; j < radix; j++) {
-            countArray[j] += countArray[j - 1];
-        }
-
-        int[] temp = new int[numItems];
-        for (int tempIndex = numItems - 1; tempIndex >= 0; tempIndex--) {
-            temp[--countArray[getDigit(position, input[tempIndex], radix)]] =
-                    input[tempIndex];
-        }
-
-        for (int tempIndex = 0; tempIndex < numItems; tempIndex++) {
-            input[tempIndex] = temp[tempIndex];
-        }
-
-    }
-
-
-    public static int getDigit(int position, int value, int radix) {
-        return value / (int) Math.pow(radix, position) % radix;
-    }
-
-
 
 }
