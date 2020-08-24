ae8b329edb9bd2cf64d0a20ef1f6deeaa1f93e7d
Zun <zunsamuel97@gmail.com>
2020-08-24 00:17:02 -0500

    merge sort

--- src/com/zun/Main.java
+++ src/com/zun/Main.java
@@ -8,56 +8,43 @@ public class Main {
 
         int[] intArr = {20, 35, -15, 7, 55, 1, -22};
 
-        for (int gap = intArr.length / 2; gap > 0; gap /= 2) {
-
-
-            for (int i = gap; i < intArr.length; i++) {
-                int newElement = intArr[i];
-
-                int j = i;
-
-                while (j >= gap && intArr[j - gap] > newElement) {
-                    intArr[j] = intArr[j - gap];
-                    j -= gap;
-                }
-
-                intArr[j] = newElement;
-
-
-            }
-
-        }
-
+        mergeSort(intArr, 0, intArr.length);
 
         System.out.println(Arrays.toString(intArr));
     }
 
-//    private static void mergeSort(int[] input, int start, int end){
-//        if(end - start < 2){
-//            return;
-//        }
-//
-//        int mid = (start + end) / 2;
-//
-//        mergeSort(input, start, mid);
-//        mergeSort(input, mid, end);
-//        merge(input, start, mid, end);
-//    }
-//
-//    private static void merge(int[] input, int start, int mid, int end) {
-//        if(input[mid - 1] <= input[mid]) return;
-//
-//        int i = start;
-//        int j = end;
-//        int tempIndex = 0;
-//
-//        int[] temp = new int[end - start];
-//        while(i < mid && j < end){
-//            temp[tempIndex++] = input[i] <= input[j] ? input[i++] : input[j++];
-//        }
-//
-//        System.arraycopy(input, i, input, start + tempIndex, mid - i);
-//        System.arraycopy(temp, 0, input, start, tempIndex);
-//    }
+    public static void mergeSort(int[] input, int start, int end) {
+
+        if (end - start < 2) {
+            return;
+        }
+
+        int mid = (start + end) / 2;
+        mergeSort(input, start, mid);
+        mergeSort(input, mid, end);
+        merge(input, start, mid, end);
+    }
+
+    // { 20, 35, -15, 7, 55, 1, -22 }
+    public static void merge(int[] input, int start, int mid, int end) {
+
+        if (input[mid - 1] <= input[mid]) {
+            return;
+        }
+
+        int i = start;
+        int j = mid;
+        int tempIndex = 0;
+
+        int[] temp = new int[end - start];
+        while (i < mid && j < end) {
+            temp[tempIndex++] = input[i] <= input[j] ? input[i++] : input[j++];
+        }
+
+        System.arraycopy(input, i, input, start + tempIndex, mid - i);
+        System.arraycopy(temp, 0, input, start, tempIndex);
+
+
+    }
 
 }
