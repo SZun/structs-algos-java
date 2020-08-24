1dfeb6add1ed39ef288f58b38f10ed3901413ea6
Zun <zunsamuel97@gmail.com>
2020-08-24 05:20:44 -0500

    iterative and recursive binary search

--- src/com/zun/Main.java
+++ src/com/zun/Main.java
@@ -1,11 +1,27 @@
 package com.zun;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	    int[] arr = {20, 35, -15, 7, 55, 1, -22};
+	    int[] arr = {-22, -15, 1, 7, 20, 35, 55};
+        System.out.println(recursiveBinarySearch(arr, -15));
+    }
 
-        System.out.println(linearSearch(arr, -15));
+    private static int iterativeBinSearch(int[] arr, int item){
+        int start = 0;
+        int end = arr.length;
+
+        while (start < end){
+            int midpoint = (start + end) / 2;
+
+            if(arr[midpoint] == item) return midpoint;
+            else if(arr[midpoint] < item) start = midpoint + 1;
+            else end = midpoint;
+        }
+
+        return -1;
     }
 
     private static int linearSearch(int[] arr, int value){
@@ -13,5 +29,27 @@ public class Main {
             if(arr[i] == value) return i;
         }
         return -1;
+    }
+
+    public static int recursiveBinarySearch(int[] input, int value) {
+        return recursiveBinarySearch(input, 0, input.length, value);
+    }
+
+    public static int recursiveBinarySearch(int[] input, int start, int end, int value) {
+        if (start >= end) {
+            return -1;
+        }
+
+        int midpoint = (start + end) / 2;
+
+        if (input[midpoint] == value) {
+            return midpoint;
+        }
+        else if (input[midpoint] < value) {
+            return recursiveBinarySearch(input, midpoint + 1, end, value);
+        }
+        else {
+            return recursiveBinarySearch(input, start, midpoint, value);
+        }
     }
 }
