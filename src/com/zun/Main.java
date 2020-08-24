a4ec28b98ea46b8958fac25ba18cff965e7ed82a
Zun <zunsamuel97@gmail.com>
2020-08-23 22:02:10 -0500

    completed bubble-sort

--- src/com/zun/Main.java
+++ src/com/zun/Main.java
@@ -1,8 +1,29 @@
 package com.zun;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int[] intArr = {20,35,-15,7,1,-22};
+
+        for(int i = intArr.length -1; i > 0; i--){
+            for(int j = 0; j < i; j++){
+                if(intArr[j] > intArr[j+1]){
+                    swap(intArr, j, j+1);
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(intArr));
+
+    }
+
+    private static void swap(int[] arr, int i, int j){
+        if(i == j) return;
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
