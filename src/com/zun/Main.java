45d946ce35246e74c765230a51e90c43ff47beb1
Zun <zunsamuel97@gmail.com>
2020-08-24 02:09:19 -0500

    array list

--- src/com/zun/Main.java
+++ src/com/zun/Main.java
@@ -1,8 +1,24 @@
 package com.zun;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(new Employee(123, "Jane", "Jones"));
+        employeeList.add(new Employee(4567, "John", "Doe"));
+        employeeList.add(new Employee(22, "Mary", "Smith"));
+        employeeList.add(new Employee(3245, "Mike", "Wilson"));
+
+        employeeList.add(3, new Employee(4567, "John", "Doe"));
+
+//        employeeList.forEach(System.out::println);
+//        System.out.println(employeeList.get(1));
+
+        Employee[] employees = employeeList.toArray(Employee[]::new);
+        System.out.println(Arrays.toString(employees));
     }
 }
