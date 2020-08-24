72a66a0140debf1ac7a40c357da21d480a22dbf0
Zun <zunsamuel97@gmail.com>
2020-08-24 05:00:45 -0500

    queue chall

--- src/com/zun/Main.java
+++ src/com/zun/Main.java
@@ -1,35 +1,23 @@
 package com.zun;
 
+import java.util.LinkedList;
+
 public class Main {
 
     public static void main(String[] args) {
-        Employee janeJones = new Employee("Jane", "Jones", 123);
-        Employee johnDoe = new Employee("John", "Doe", 4567);
-        Employee marySmith = new Employee("Mary", "Smith", 22);
-        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
-        Employee billEnd = new Employee("Bill", "End", 78);
+        System.out.println(isPalindrome("racecar"));
+        System.out.println(isPalindrome("not a palindrome"));
+    }
 
-        ArrayQueue queue = new ArrayQueue(10);
-        queue.add(janeJones);
-        queue.add(johnDoe);
-        queue.add(marySmith);
-        queue.add(mikeWilson);
-        queue.add(billEnd);
-        //queue.printQueue();
+    private static boolean isPalindrome(String s){
+        LinkedList<Character> stack = new LinkedList();
+        LinkedList<Character> queue = new LinkedList();
 
-        queue.remove();
-        queue.remove();
-        //queue.printQueue();
+        for(char i : s.toLowerCase().toCharArray()){
+            queue.addLast(i);
+            stack.push(i);
+        }
 
-        //System.out.println(queue.peek());
-        queue.remove();
-        queue.remove();
-        queue.remove();
-        //queue.remove();
-
-        queue.add(mikeWilson);
-
-
-        queue.printQueue();
+        return stack.equals(queue);
     }
 }
