7319cca2eec2d53b0ecfc55defdac4e376253d48
Zun <zunsamuel97@gmail.com>
2020-08-24 00:14:37 -0500

    shell sort

--- src/com/zun/Main.java
+++ src/com/zun/Main.java
@@ -6,20 +6,58 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] intArr = {20,35,-15,7,1,-22};
+        int[] intArr = {20, 35, -15, 7, 55, 1, -22};
 
-        for(int i = 1; i < intArr.length; i++){
-            int elNew = intArr[i];
-            int j;
+        for (int gap = intArr.length / 2; gap > 0; gap /= 2) {
 
-            for(j = i; j > 0 && intArr[j - 1] > elNew; j--){
-                intArr[j] = intArr[j - 1];
+
+            for (int i = gap; i < intArr.length; i++) {
+                int newElement = intArr[i];
+
+                int j = i;
+
+                while (j >= gap && intArr[j - gap] > newElement) {
+                    intArr[j] = intArr[j - gap];
+                    j -= gap;
+                }
+
+                intArr[j] = newElement;
+
+
             }
 
-            intArr[j] = elNew;
         }
+
 
         System.out.println(Arrays.toString(intArr));
     }
+
+//    private static void mergeSort(int[] input, int start, int end){
+//        if(end - start < 2){
+//            return;
+//        }
+//
+//        int mid = (start + end) / 2;
+//
+//        mergeSort(input, start, mid);
+//        mergeSort(input, mid, end);
+//        merge(input, start, mid, end);
+//    }
+//
+//    private static void merge(int[] input, int start, int mid, int end) {
+//        if(input[mid - 1] <= input[mid]) return;
+//
+//        int i = start;
+//        int j = end;
+//        int tempIndex = 0;
+//
+//        int[] temp = new int[end - start];
+//        while(i < mid && j < end){
+//            temp[tempIndex++] = input[i] <= input[j] ? input[i++] : input[j++];
+//        }
+//
+//        System.arraycopy(input, i, input, start + tempIndex, mid - i);
+//        System.arraycopy(temp, 0, input, start, tempIndex);
+//    }
 
 }
